8e39b215c035b049de60f31b2df9b1152d011595
lawrenceliicosa <lawrenceli@icosacomputing.com>
2025-06-18 23:42:19 +0800

    Added main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# A. A commented summary of your platform shortlist with pros/cons.
+# B. A Python function that, given any U.S. ticker symbol (e.g. AAPL, TSLA), fetches the most recent daily OHLC(+adj_close) and volume data up to the latest trading date, along with a historical period of data required.
+# C. (Optional) Notes or code snippets showing how you would fetch 1-minute data if available.
+# D. Any necessary visualizations/plottings for demonstration purpose.
+
+da
