4b7e0f82c93ec0f329956be1e500220558200a34
Lawrence2005 <lawrenceee0126@gmail.com>
2025-06-19 09:38:31 +0800

    Changed function signature

--- Financial-Data-API-Research-Week1/main.py
+++ Financial-Data-API-Research-Week1/main.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 # A. A commented summary of your platform shortlist with pros/cons.
 
-# B. A Python function that, given any U.S. ticker symbol (e.g. AAPL, TSLA), fetches the most recent daily OHLC(+adj_close) and volume data up to the latest trading date, along with a historical period of data required.
-def fetch_daily_data(ticker: str, start_date: str, end_date: str, source: str = 'tiingo') -> pd.DataFrame:
+# A Python function that, given any U.S. ticker symbol (e.g. AAPL, TSLA), fetches the most recent daily OHLC(+adj_close) and volume data up to the latest trading date, along with a historical period of data required.
+def fetch_daily_data(ticker: str, num_days: int, source: str = 'tiingo') -> pd.DataFrame:
     pass
 
 # C. (Optional) Notes or code snippets showing how you would fetch 1-minute data if available.
